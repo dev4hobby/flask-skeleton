acd39a06b201b80a4672f31785c0b2d8c9ddb01c
d3fau1t <aciddust20@gmail.com>
2021-10-31 10:10:58 +0900

    :pencil2: rid off, duplication
    get_collection

--- database/user.py
+++ database/user.py
@@ -1,29 +1,29 @@
 from database import db
 from utils.auth import generate_key
 
+user = db.get_collection("user")
+
 
 def set_user(provider: str, info: dict) -> bool:
     # provider like google, facebook, kakao.. etc..
     auth = generate_key(64)
-    result = db.get_collection("user").insert_one(
-        {"provider": provider, "auth": auth, **info}
-    )
+    result = user.insert_one({"provider": provider, "auth": auth, **info})
     return auth
 
 
 def get_user_by_user_id(provider: str, user_id: str) -> dict:
     # provider like google, facebook, kakao.. etc..
-    result = db.get_collection("user").find_one({"id": user_id}, {"_id": False})
+    result = user.find_one({"id": user_id}, {"_id": False})
     return result
 
 
 def get_user_by_user_auth(auth: str) -> dict:
-    result = db.get_collection("user").find_one({"auth": auth}, {"_id": False})
+    result = user.find_one({"auth": auth}, {"_id": False})
     return result
 
 
 def get_users_by_email(email: str) -> list:
-    result = db.get_collection("user").find(
+    result = user.find(
         {"email": {"$regex": f".*{email}.*"}},
         {"_id": False},
     )
@@ -31,7 +31,7 @@ def get_users_by_email(email: str) -> list:
 
 
 def get_users_by_name_with_email(name: str, email: str) -> list:
-    result = db.get_collection("user").find(
+    result = user.find(
         {
             "$and": [
                 {"name": name},
