94e39e658eadfee6bbc15e122f2aeaf9c68459a1
d3fau1t <aciddust20@gmail.com>
2021-11-18 23:51:51 +0900

    :art: application factory pattern
    ?

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,26 +1,29 @@
-from flask import Flask
-from flask_cors import CORS
-from utils.env import get_database_uri
-from utils.auth import generate_key
-from settings import *
-from database import db
-from . import home, misc, wtfLab, redisLab
+def create_app():
+    from flask import Flask
+    from flask_cors import CORS
+    from utils.env import get_database_uri
+    from utils.auth import generate_key
+    from settings import DATABASES
+    from database import db
+    from . import home, misc, wtfLab, redisLab
 
-app = Flask(__name__)
-app.secret_key = generate_key(16)
+    app = Flask(__name__)
+    app.secret_key = generate_key(16)
 
-app.config["MONGODB_SETTINGS"] = {
-    "db": DATABASES.get("name"),
-    "host": get_database_uri(DATABASES),
-    "port": int(DATABASES.get("port")),
-    "username": DATABASES.get("username"),
-    "password": DATABASES.get("password"),
-}
-db.init_app(app)
+    app.config["MONGODB_SETTINGS"] = {
+        "db": DATABASES.get("name"),
+        "host": get_database_uri(DATABASES),
+        "port": int(DATABASES.get("port")),
+        "username": DATABASES.get("username"),
+        "password": DATABASES.get("password"),
+    }
+    db.init_app(app)
 
-app.register_blueprint(home.home)
-app.register_blueprint(misc.misc)
-app.register_blueprint(wtfLab.wtf)
-app.register_blueprint(redisLab.redis_lab)
+    app.register_blueprint(home.home)
+    app.register_blueprint(misc.misc)
+    app.register_blueprint(wtfLab.wtf)
+    app.register_blueprint(redisLab.redis_lab)
 
-CORS(app, support_credentials=True)
+    CORS(app, support_credentials=True)
+
+    return app
